19c786d4e4f08111058368bc46b7638cc0325732
yuichi-kawamura <kawamura@axrobotix.com>
2024-03-14 18:26:34 +0900

    fist commit

--- lixx.cpp
+++ lixx.cpp
@@ -19,20 +19,12 @@ const int Pin_leftMotorIn1 =  26;
 const int Pin_leftMotorIn2 =  25;
 const int Pin_rightMotorIn1 = 32;
 const int Pin_rightMotorIn2 = 33;
-
+  
 //for PhotoReflector
 const int Pin_leftSensor = 36;
 const int Pin_rightSensor = 39;
 
 //// Function Declarations ////
-
-void forward()
-{
-  digitalWrite(Pin_rightMotorIn1, 0);
-  digitalWrite(Pin_rightMotorIn2, 1);
-  digitalWrite(Pin_leftMotorIn1, 0);
-  digitalWrite(Pin_leftMotorIn2, 1);
-}
 
 void forward(int duty_r, int duty_l)
 {
@@ -41,34 +33,27 @@ void forward(int duty_r, int duty_l)
   digitalWrite(Pin_rightMotorIn1, 0);
   digitalWrite(Pin_leftMotorIn1, 0);
 }
-/*
-  ledcAttachPin(Pin_leftMotorIn1, 0);  //0ch setting
-  ledcAttachPin(Pin_leftMotorIn2, 1);  //1ch setting
-  ledcAttachPin(Pin_rightMotorIn1, 2);  //2ch setting
-  ledcAttachPin(Pin_rightMotorIn2, 3);  //3ch setting
-*/
-
-void backward()
+void backward(int duty_r, int duty_l)
 {
-  digitalWrite(Pin_rightMotorIn1, 1);
+  ledcWrite(2, duty_r);
+  ledcWrite(0, duty_l);
   digitalWrite(Pin_rightMotorIn2, 0);
-  digitalWrite(Pin_leftMotorIn1, 1);
   digitalWrite(Pin_leftMotorIn2, 0);
 }
 
-void left_turn()
+void left_turn(int duty_r, int duty_l)
 {
-  digitalWrite(Pin_rightMotorIn1, 1);
+  ledcWrite(3, duty_r);
+  ledcWrite(0, duty_l);
   digitalWrite(Pin_rightMotorIn2, 0);
   digitalWrite(Pin_leftMotorIn1, 0);
-  digitalWrite(Pin_leftMotorIn2, 1);  
 }
 
-void right_turn()
+void right_turn(int duty_r, int duty_l)
 {
+  ledcWrite(2, duty_r);
+  ledcWrite(1, duty_l);
   digitalWrite(Pin_rightMotorIn1, 0);
-  digitalWrite(Pin_rightMotorIn2, 1);
-  digitalWrite(Pin_leftMotorIn1, 1);
   digitalWrite(Pin_leftMotorIn2, 0);  
 }
 
@@ -90,21 +75,43 @@ void brake(int time_ms)
   delay(time_ms);  
 }
 
+void motorTest(int duty)
+{
+  //左右（白白）→直進
+  forward(duty, duty);
+  delay(500);
+  brake(250);
+
+  //左右（白白）→後進
+  backward(duty, duty);
+  delay(500);
+  brake(250);  
+
+  //左右（黒白）→右旋回（左モータ正転）
+  right_turn(duty, duty);
+  delay(500);
+  brake(250);  
+
+  //左右（白黒）→左旋回（右モータ正転）
+  left_turn(duty, duty);
+  delay(500);
+  brake(250);  
+}
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("lixx startup"); 
+  Serial.println("void setup passed"); 
 
   pinMode(Pin_leftMotorIn1, OUTPUT);
   pinMode(Pin_leftMotorIn2, OUTPUT);
   pinMode(Pin_rightMotorIn1, OUTPUT);
   pinMode(Pin_rightMotorIn2, OUTPUT);
 
-  ledcSetup(0, 12800, 10); //0ch, 10kHz, 10bit(1024)Resolution
-  ledcSetup(1, 12800, 10); //0ch, 10kHz, 10bit(1024)Resolution
-  ledcSetup(2, 12800, 10); //0ch, 10kHz, 10bit(1024)Resolution
-  ledcSetup(3, 12800, 10); //0ch, 10kHz, 10bit(1024)Resolution
+  ledcSetup(0, 12800, 10); //0ch, 12,8kHz, 10bit(1024)Resolution
+  ledcSetup(1, 12800, 10); //0ch, 12,8kHz, 10bit(1024)Resolution
+  ledcSetup(2, 12800, 10); //0ch, 12,8kHz, 10bit(1024)Resolution
+  ledcSetup(3, 12800, 10); //0ch, 12,8kHz, 10bit(1024)Resolution
   ledcAttachPin(Pin_leftMotorIn1, 0);  //0ch setting
   ledcAttachPin(Pin_leftMotorIn2, 1);  //1ch setting
   ledcAttachPin(Pin_rightMotorIn1, 2);  //2ch setting
@@ -113,6 +120,31 @@ void setup()
 
 void loop()
 {
+  Serial.println("void loop passed"); 
+  //モータの正常動作を確認（前後左右）
+  //motorTest();
+
+  //左右（白白）→直進
+  forward(1023, 1023);
+  delay(500);
+  brake(250);
+
+  //左右（白白）→後進
+  backward(1023, 1023);
+  delay(500);
+  brake(250);  
+
+  //左右（黒白）→右旋回（左モータ正転）
+  right_turn(1023, 1023);
+  delay(500);
+  brake(250);  
+
+  //左右（白黒）→左旋回（右モータ正転）
+  left_turn(1023, 1023);
+  delay(500);
+  brake(250);  
+/*
+
   static int duty = 0;
   static int diff = 1;
   
@@ -145,29 +177,5 @@ void loop()
   Serial.print("rightSensor: ");
   Serial.println(analogValueR);
   Serial.println();
-
-/*
-  //左右（白白）→直進
-  forward();
-  delay(500);
-  brake(250);
-
-  //左右（白白）→後進
-  backward();
-  delay(500);
-  brake(250);  
-
-  //左右（黒白）→右旋回（左モータ正転）
-  right_turn();
-  delay(500);
-  brake(250);  
-
-  //左右（白黒）→左旋回（右モータ正転）
-  left_turn();
-  delay(500);
-  brake(250);  
-
-  delay(1000);
-  */
-
+*/
 }
